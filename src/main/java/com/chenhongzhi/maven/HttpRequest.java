a3a75077e78e55b5746c3dd25e48dbcddbe4f6b3
mavenTestNGdemo <862365198@qq.com>
2022-04-09 18:30:56 +0800

    demo1

--- src/main/java/com/chenhongzhi/maven/HttpRequest.java
+++ src/main/java/com/chenhongzhi/maven/HttpRequest.java
@@ -1,20 +1,21 @@
 package com.chenhongzhi.maven;
 
 
-import org.apache.http.Header;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import jdk.nashorn.api.scripting.ScriptUtils;
+import jdk.nashorn.internal.runtime.JSONFunctions;
+import jdk.nashorn.internal.runtime.JSONListAdapter;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.testng.Assert;
 
-import javax.swing.text.html.parser.Entity;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
 
 public class HttpRequest {
     /**
@@ -27,12 +28,12 @@ public class HttpRequest {
      * @param url
      * @return httpresponse
      */
-    public static String sentGet(String url) throws IOException {
-        //创建实例
+    public static JSONObject sentGet(String url) throws IOException {
+        //创建请求实例
         HttpGet httpGet = new HttpGet(url);
-        System.out.println("sendGet:"+url);
-        String content = null;
-        //加headers
+//        System.out.println("sendGet:"+url);
+        JSONObject content = null;
+        //加入参
         httpGet.setHeader("cookie", "PHPSESSID=it13e8gm7mecrfiq9lm8nrm0q5; Hm_lvt_ca368c21c1d2aa60e6f63d598c4cb02a=1649486265; Hm_lpvt_ca368c21c1d2aa60e6f63d598c4cb02a=1649488617");
         httpGet.setHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
         httpGet.setHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36");
@@ -41,15 +42,16 @@ public class HttpRequest {
         try {
             //entity类获取
             HttpEntity httpEntity = closeableHttpResponse.getEntity();
-            System.out.println("打印返回内容：====================================");
+//            System.out.println("打印返回内容：====================================");
             String contenttype = String.valueOf(httpEntity.getContentType());
-            System.out.println("contenttype:"+contenttype);
-            content = EntityUtils.toString(httpEntity);
-            System.out.println("content:"+content);
+//            System.out.println("contenttype:"+contenttype);
+            content = (JSONObject) JSONArray.parse(EntityUtils.toString(httpEntity));
+//            System.out.println("content:"+content);
         }catch (Exception e){
             e.printStackTrace();
         }
         finally {
+            //关闭流
             closeableHttpResponse.close();
         }
         return content;
@@ -57,7 +59,8 @@ public class HttpRequest {
 
 
     //测试方法
-    public static void main(String[] args) throws IOException {
-        System.out.println(HttpRequest.sentGet("https://www.iamwawa.cn/home/lizhi/ajax"));
-    }
+//    public static void main(String[] args) throws IOException {
+//        JSONObject content = HttpRequest.sentGet("https://www.iamwawa.cn/home/lizhi/ajax");
+//        Assert.assertEquals(content.get("data"),"我爱你");
+//    }
 }
---
+++ src/test/java/com/chenhongzhi/maven/HttpRequestTest.java
@@ -0,0 +1,59 @@
+package com.chenhongzhi.maven;
+
+import com.alibaba.fastjson.JSONObject;
+import jdk.Exported;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.*;
+
+import java.io.IOException;
+
+public class HttpRequestTest {
+
+    @BeforeMethod
+    public String[] setUp() {
+        String[] string123 = {"https://www.iamwawa.cn/home/lizhi","https://www.iamwawa.cn/home/lizhi/ajax"};
+        System.out.println("@BeforeMethod");
+        //使用before传参,声明该方法的返回值类型并返回相应数据
+        return string123;
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        System.out.println("@AfterMethod");
+    }
+
+    //1.使用testNG.xml传参，并右键xml文件run
+//    @Test(timeOut = 800,enabled = true)
+//    @Parameters({"url"})
+//    public void testSentGet(String url) {
+//        System.out.println("@Test"+url);
+//        try {
+//            JSONObject json = HttpRequest.sentGet(url);
+//            String expectInfo = "查询成功！";
+//            System.out.println("expectInfo:"+expectInfo);
+//            String actualInfo = (String) json.get("info");
+//            System.out.println("actualInfo:"+actualInfo);
+//            Assert.assertEquals(expectInfo,actualInfo);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//            Reporter.log(e.getMessage());
+//        }
+//    }
+
+
+    //2.使用before传递参数，setUp方法返回数据并在test中引用
+    @Test(expectedExceptions = Exception.class)
+    public void testSentGet() throws Exception {
+        System.out.println("@Test");
+        JSONObject json = HttpRequest.sentGet(setUp()[1]);
+        String expectInfo = "查询成功！";
+        System.out.println("expectInfo:"+expectInfo);
+        String actualInfo = (String) json.get("info");
+        System.out.println("actualInfo:"+actualInfo);
+        Assert.assertEquals(expectInfo,actualInfo);
+        throw new Exception();
+
+    }
+
+}
