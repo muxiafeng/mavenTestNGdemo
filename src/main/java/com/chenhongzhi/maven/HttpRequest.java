5ae90528d7c02fb6afed22268f330d16d576b493
mavenTestNGdemo <862365198@qq.com>
2022-04-11 21:14:03 +0800

    顶顶顶

--- src/main/java/com/chenhongzhi/maven/HttpRequest.java
+++ src/main/java/com/chenhongzhi/maven/HttpRequest.java
@@ -34,9 +34,9 @@ public class HttpRequest {
 //        System.out.println("sendGet:"+url);
         JSONObject content = null;
         //加入参
-        httpGet.setHeader("cookie", "PHPSESSID=it13e8gm7mecrfiq9lm8nrm0q5; Hm_lvt_ca368c21c1d2aa60e6f63d598c4cb02a=1649486265; Hm_lpvt_ca368c21c1d2aa60e6f63d598c4cb02a=1649488617");
-        httpGet.setHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-        httpGet.setHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36");
+        httpGet.setHeader("cookie", "PHPSESSID=5m8ltehht21qcb789u6808sgp7; Hm_lvt_ca368c21c1d2aa60e6f63d598c4cb02a=1649486265,1649603913; Hm_lpvt_ca368c21c1d2aa60e6f63d598c4cb02a=1649603918");
+        httpGet.setHeader("Accept","application/json, text/javascript, */*; q=0.01");
+        httpGet.setHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36");
         //发送get请求
         CloseableHttpResponse closeableHttpResponse = (CloseableHttpResponse) httpClient.execute(httpGet);
         try {
@@ -46,7 +46,7 @@ public class HttpRequest {
             String contenttype = String.valueOf(httpEntity.getContentType());
 //            System.out.println("contenttype:"+contenttype);
             content = (JSONObject) JSONArray.parse(EntityUtils.toString(httpEntity));
-//            System.out.println("content:"+content);
+            System.out.println("content:"+content);
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -59,8 +59,8 @@ public class HttpRequest {
 
 
     //测试方法
-//    public static void main(String[] args) throws IOException {
-//        JSONObject content = HttpRequest.sentGet("https://www.iamwawa.cn/home/lizhi/ajax");
+    public static void main(String[] args) throws IOException {
+        JSONObject content = HttpRequest.sentGet("https://www.iamwawa.cn/home/lizhi/ajax");
 //        Assert.assertEquals(content.get("data"),"我爱你");
-//    }
+    }
 }
